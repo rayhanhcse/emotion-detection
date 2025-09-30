7fae025f26a4a10d94d7c25834f413c239276fb6
Rayhan Hussain <135307421+rayhanhcse@users.noreply.github.com>
2025-10-01 00:55:49 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+import cv2  # must work now with headless version
+import numpy as np
 import streamlit as st
-import cv2
+from tensorflow.keras.models import load_model
+from PIL import Image
 import os
+
 
 cascade_path = os.path.join("cascades", "haarcascade_frontalface_default.xml")
 face_cascade = cv2.CascadeClassifier(cascade_path)
@@ -79,6 +83,7 @@ else:
             cv2.putText(frame, emotion, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255,0,0), 2)
 
         st.image(frame, channels="RGB")
+
 
 
 
